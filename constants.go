57d04c5cba2c48779f8518018da6f1cfaf7f09ab
caiopo <caiopoliveira@gmail.com>
2016-04-20 19:43:11 -0300

    Fixed an uppercase character

--- constants.go
+++ constants.go
@@ -1,10 +1,8 @@
 package pontoon
 
 const (
-	kubernetesAPIServer = "192.168.1.200:8080"
+	KubernetesAPIServer = "192.168.1.200:8080"
 	// KubernetesAPIServer = "192.168.15.150:8080"
-	RunningInKubernetes = true
+	RunningInKubernetes = false
 	PORT                = ":55123"
 )
-
-var ValidPorts = []string{":55125", ":55126", ":55127", ":55128", ":55129", ":55130"}
